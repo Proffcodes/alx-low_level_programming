280d050a35dc1a3dd7873f010b008b125a7d0558
michael olalekan paul <olalekan.paul2010@gmail.com>
2022-09-08 20:13:08 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program.
+ *
+ * Author- Michael Olalekan Paul
+ *
+ * Return:0 if no errors, return non zero if errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
