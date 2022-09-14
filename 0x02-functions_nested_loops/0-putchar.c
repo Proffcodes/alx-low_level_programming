d301bbc24a7d21e3c49164870375f8c76505620d
michael olalekan paul <olalekan.paul2010@gmail.com>
2022-09-13 20:07:27 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,28 +2,22 @@
 
 /**
  * main - print putchar
- *
  * Return: always 0
- *
  */
 
-
-
 int main(void)
-
 {
-	 putchar('_');
- 	 putchar('p');
-	 putchar('u');
-	 putchar('t');
-	 putchar('c');
-	 putchar('h');
-	 putchar('a');
-	 putchar('r');
-	 putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 
-
-
 }
+
